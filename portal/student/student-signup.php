ffdb31e68baf0a8b105bb5a5ac709b0d66d2448c
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2024-06-20 17:38:38 +0000

    Second commit
    2

--- portal/student/student-signup.php
+++ portal/student/student-signup.php
@@ -2,5 +2,22 @@
 
 require_once('../../scripts/conn.php');
 
+if(isset($_POST['submit'])){
+    $firstName= $_POST['firstname'];
+    $lastName= $_POST['lastname'];
+    $dob= $_POST['Dob'];
+    $email= $_POST['email'];
+    $contact= $_POST['contact'];
+    $username= $_POST['username']; 
+    $password= $_POST['password1'];
 
+    $sql= "INSERT INTO student (id, firstName, lastName, dob, email, contact, userName, password1) 
+    VALUES('0','$firstName', '$lastName', '$dob', '$email', '$username', '$password')";
+
+    $rsl= mysqli_query($conn,$sql);
+
+    if(!$rsl){
+        
+    }
+}
  ?>
