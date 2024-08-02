cdd91f015191458a3ddbfce3ccce480c1a470853
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2024-08-02 17:32:44 +0000

    Twentyfirst
    21

--- portal/teacher/teacher-signup.php
+++ portal/teacher/teacher-signup.php
@@ -1,3 +1,16 @@
 <?php
     require_once('../../scripts/conn.php');
+
+    if(isset($_POST['submit'])){
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $date_of_birth = $_POST['Dob'];
+        $username = $_POST['username'];
+        $password = $_POST['password1'];
+        $email = $_POST['email'];
+        $contact = $_POST['contact'];
+        $description = $_POST['description'];
+        $cert = $_POST['Cert'];
+        
+    }
 ?>
