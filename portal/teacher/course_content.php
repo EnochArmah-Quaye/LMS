82d42a0873aa74582d8c476e8dcdf08b639fb54e
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2024-11-12 05:51:29 +0000

    Finished working on addin course content to courses
    Yep

---
+++ portal/teacher/course_content.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('../../scripts/conn.php');
+
+if(isset($_POST['submit'])){
+    $course_id = $_POST['course_id'];
+    $intro = $_POST['intro'];
+    $lesson_1 = $_POST['lesson1'];
+    
+    $lesson_2 = $_POST['lesson2'];
+    
+
+    $resources = ['resource1','resource2'];
+
+    $files = [];
+    
+    foreach ($resources as $resource) {
+       $file_name = $_FILES[$resource]['name'];
+       $file_tmp = $_FILES[$resource]['tmp_name'];
+       $file_size = $_FILES[$resource]['size'];
+       $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+
+       if($file_ext !== 'pdf'){
+        echo "<div>File must be a pdf</div>";  
+          }
+       else{
+        $target_path ="../../pdf/" . basename($file_name); 
+       
+        if(move_uploaded_file($file_tmp, $target_path)){
+            $files [] = $target_path ; 
+        }
+       else{
+        echo "<div> File upload Failed </div>";
+       }
+       }
+    }
+
+
+    $stmt = $conn->prepare("INSERT INTO course_content (course_id, intro, lesson1, resource1, lesson2, resource2) VALUES(?,?,?,?,?,?)");
+    $stmt->bind_param("isssss", $course_id, $intro, $lesson_1, $files[0], $lesson_2, $files[1]);
+    
+    if($stmt->execute()){
+        echo "<div>Course Added Successfully</div>";
+    }
+
+    
+}
+?>
--- portal/teacher/teacher-signup.php
+++ portal/teacher/teacher-signup.php
@@ -1,65 +1,7 @@
 <?php
     require_once('../../scripts/conn.php');
 
-   /* if(isset($_POST['submit'])){
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        $date_of_birth = $_POST['Dob'];
-        $username = $_POST['username'];
-        $password = $_POST['password1'];
-        $email = $_POST['email'];
-        $contact = $_POST['contact'];
-        $description = $_POST['description'];
-
-        echo "<div>This part works fine</div>";
-       
-       if(isset($_FILES['Cert']))
-       {
-        //Check if the Cert field has an attachment
-          $file_name = $_FILES['Cert'];
-          $file_tmp = $_FILES['Cert']['tmp_name'];
-
-          //Move the uploaded file into the pdf folder
-          move_uploaded_file($file_tmp,"../../pdf/".$file_name);
-
-          $insertquery = "INSERT INTO teacher (firstName,lastName,dob,userName,password2,email,contact,description1,file1)
-           VALUES('$firstname','$lastname','$date_of_birth','$username','$password','$email','$contact','$description','$file_name')";
-
-           $result = mysqli_query($conn,$insertquery);
-
-           if($result){
-            echo"<html lang='en'>
-            <head>
-                <meta charset='UTF-8'>
-                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-                <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                <title>Document</title>
-                <link rel='stylesheet' href='../../dist/css/main.css'>
-                <script src='../../js/bootstrap.bundle.min.js'></script>
-            </head>
-            <body>
-               <div class='alert alert-success' role='alert'>
-                 <a class='close' data-dismiss='alert' aria-label='close'>x</a>
-                 <strong>Success!</strong> Data submitted successfully.
-               </div>
-            </body>
-            </html>";
-           }
-           else{
-            echo "<div class='alert alert-danger' role='alert'>
-                    <a class='close' data-dismiss='alert' araia-label='close'>x</a>
-                    <strong>Failed!</strong> Try Again
-            </div>";
-           }
-       } 
-       else{
-         echo "<div class='alert alert-danger' role='alert'>
-               <a class='close' data-dismiss='alert' aria-label='close'>x</a>
-               <strong>Failed!</strong> File must be uploaded in PDF Format!
-         </div>";
-        }
-
-    }*/
+   
 
     if(isset($_POST['submit'])){
       $firstname = $_POST['firstname'];
